14516f573a6600c8dec33e9aeca636d44bf15a21
JWabbit017 <janszen@galactis-group.com>
2025-01-23 14:43:33 +0100

    Gripsum V1.1

--- subdirectories/gripsum/js/string_gen.js
+++ subdirectories/gripsum/js/string_gen.js
@@ -44,59 +44,98 @@ const selection = [
     "handige",
     "extra functies",
     "met",
-    "op het aanrecht"
+    "op het aanrecht",
+    "altijd perfecte",
+    "maaltijden"
 ];
 
-var max = selection.length;
-var min = 0;
-var main = document.getElementById("test");
-var title = document.getElementById("title");
-var genbutton = document.getElementById("generate");
-var linetwo = document.getElementById("linetwo");
-var line3 = document.getElementById("linethree");
-var line4 = document.getElementById("linefour");
-var line5 = document.getElementById("linefive");
-const timesGenerated = 0;
+const main = document.getElementById("test");
+const title = document.getElementById("title");
+const genbutton = document.getElementById("generate");
+const button = document.querySelector("#container > header > button");
+let isDarkMode = false;
 
-function ChooseArrayStr() 
+
+function ChooseArrayStr(maxValue, minValue) 
 {
-    interCalc = Math.random() * max;
-    return (Math.round(interCalc));
+    return (Math.round(Math.random() * maxValue) + minValue);
 }
 
-function PullFromArray()
+function Pull()
 {
-    return selection[ChooseArrayStr()];
+    return selection[ChooseArrayStr(selection.length, 0)];
+}
+
+function ColorMode()
+{   
+    if (isDarkMode == false)
+    {
+        document.querySelector("main").style.backgroundColor = "rgb(60,60,60)";
+        document.querySelector("footer").style.backgroundColor = "rgb(60,60,60)";
+        document.querySelector("footer").style.color = "whitesmoke";
+        document.querySelector("#container > header").style.color = "whitesmoke";
+        document.querySelector("main").style.color = "whitesmoke";
+        document.querySelector("main > #buttons input").style.backgroundColor = "rgb(60,60,60)";
+        document.querySelector("main > #buttons button").style.backgroundColor = "rgb(60,60,60)";
+        document.querySelector("main > #about, main > #other").style.backgroundColor = "rgb(60,60,60)";
+        document.querySelector("main > #about, main > #other").style.boxShadow = "0px 6px 6px 5px rgb(90,90,90)";
+        document.querySelector("main > #other main").style.backgroundColor = "rgb(60,60,60)";
+        document.querySelector("main > #other").style.backgroundColor = "rgb(60,60,60)";
+        document.querySelector("main > #other").style.boxShadow = "0px 6px 6px 5px rgb(90,90,90)";
+        document.querySelector("main > #other section").style.backgroundColor = "rgb(70,70,70)";
+        document.querySelector("main > #other section:last-of-type").style.backgroundColor = "rgb(70,70,70)";
+        document.querySelector("#container > header button").style.color = "whitesmoke";
+        document.querySelector("#container > header button").innerHTML = "Licht Schema";
+        document.querySelector("#container > header button").style.border = "1px solid whitesmoke";
+        document.querySelector("#container > header > a").style.color = "whitesmoke";
+        isDarkMode = true;
+        console.log("Dark Mode");
+        return;
+    }
+    else
+    {
+        document.querySelector("main").style.backgroundColor = "whitesmoke";
+        document.querySelector("footer").style.backgroundColor = "rgb(230,230,230)";
+        document.querySelector("footer").style.color = "rgb(30,30,30)";
+        document.querySelector("#container > header").style.color = "rgb(30,30,30)";
+        document.querySelector("main").style.color = "rgb(30,30,30)";
+        document.querySelector("main > #buttons input").style.backgroundColor = "whitesmoke";
+        document.querySelector("main > #buttons button").style.backgroundColor = "whitesmoke";
+        document.querySelector("main > #about, main > #other").style.backgroundColor = "rgb(230,230,230)";
+        document.querySelector("main > #about, main > #other").style.boxShadow = "";
+        document.querySelector("main > #other main").style.backgroundColor = "rgb(230,230,230)";
+        document.querySelector("main > #other").style.backgroundColor = "rgb(230,230,230)";
+        document.querySelector("main > #other").style.boxShadow = "";
+        document.querySelector("main > #other section").style.backgroundColor = "lightgrey";
+        document.querySelector("main > #other section:last-of-type").style.backgroundColor = "lightgrey";
+        document.querySelector("#container > header button").style.color = "rgb(30,30,30)";
+        document.querySelector("#container > header button").innerHTML = "Donker Schema";
+        document.querySelector("#container > header button").style.border = "1px solid rgb(30,30,30)";
+        document.querySelector("#container > header > a").style.color = "rgb(30,30,30)";
+        isDarkMode == false;
+        console.log("Light Mode");
+        return;
+    }
 }
 
 function Apply()
 {
-    main.innerHTML = PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray();
-    title.innerHTML = PullFromArray();
     const amountOfLines = document.getElementById("numberinput").value;
-    if (amountOfLines == 2) {
-        linetwo.innerHTML = PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray();
-    } else linetwo.innerHTML = " ";
-    if (amountOfLines == 3) {
-        linetwo.innerHTML = PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray();
-        linethree.innerHTML = PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray();
-    } else linethree.innerHTML = " ";
-    if (amountOfLines == 4) {
-        linetwo.innerHTML = PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray();
-        linethree.innerHTML = PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray();
-        linefour.innerHTML = PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray();
-    } else linefour.innerHTML = " ";
-    if (amountOfLines == 5) {
-        linetwo.innerHTML = PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray();
-        linethree.innerHTML = PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray();
-        linefour.innerHTML = PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray();
-        linefive.innerHTML = PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray() + " " + PullFromArray();
-    } else linefive.innerHTML = " ";
-    //timesGenerated += 1;
+    let amountToGen = ChooseArrayStr(8, 3);
+    let output = new String();
+
+    for (let j = 0; j < amountOfLines; j++)
+    {
+        for (let i = 0; i < amountToGen; i++)
+            {
+                output += `${Pull()} `;
+            }
+            output += ". ";
+    }
+    let newOutput = output.replaceAll(/\s\.\s/img, ". ");
+    main.innerHTML = newOutput;
+    title.innerHTML = Pull();
 }
 
 genbutton.addEventListener("click", Apply, true);
-
-//if (timesGenerated > 10) {
-//    selection.push("A river dirt cheese");
-//}
+button.addEventListener("click", ColorMode);
