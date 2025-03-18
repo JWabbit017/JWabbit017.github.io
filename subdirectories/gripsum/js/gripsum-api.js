ee0ce3b1b627af22668480420f27752f1567af54
JWabbit017 <janszen@galactis-group.com>
2025-03-18 12:06:25 +0100

    api test 1

---
+++ subdirectories/gripsum/js/gripsum-api.js
@@ -0,0 +1,48 @@
+const selected = document.querySelectorAll(".gripsum")
+//const copyButton = document.getElementById("copy");
+const button = document.querySelector("#container > header > button");
+let selection = null;
+
+async function GetSelection()
+{
+    let response = await fetch("././resources/selection-en.json");
+    let data = await response.json();
+    console.log("If you're reading this, this page is miraculously working correctly");
+    selection = data.array.selection;
+}
+
+GetSelection();
+
+function ChooseArrayStr(maxValue, minValue) 
+{
+    return (Math.round(Math.random() * maxValue) + minValue);
+}
+
+function Pull()
+{
+    return selection[ChooseArrayStr(selection.length, 0)];
+}
+
+function Apply()
+{  
+    const amountOfLines = 2;
+    let amountToGen = ChooseArrayStr(8, 3);
+    let output = new String();
+
+    for (let j = 0; j < amountOfLines; j++)
+    {
+        for (let i = 0; i < amountToGen; i++)
+            {
+                output += `${Pull()} `;
+            }
+            output += ". ";
+    }
+    output = output.replaceAll(/\s\.\s/img, ". ");
+    return output;
+}
+
+//copyButton.addEventListener("click", CopyGenerated(main));
+for (let selection of selected)
+{
+    selection.innerHTML = Apply();
+}
